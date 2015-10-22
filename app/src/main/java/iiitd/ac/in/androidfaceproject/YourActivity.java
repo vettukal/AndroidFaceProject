ef017bcf284e9016ca03b63b0ed5dedd23034211
vettukal <vettukal@gmail.com>
2015-10-23 02:39:31 +0530

    [working] With time delay working properly. Trying to change the background color but without success.

--- app/src/main/java/iiitd/ac/in/androidfaceproject/YourActivity.java
+++ app/src/main/java/iiitd/ac/in/androidfaceproject/YourActivity.java
@@ -1,6 +1,8 @@
 package iiitd.ac.in.androidfaceproject;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
 
 import com.daimajia.androidanimations.library.Techniques;
@@ -40,7 +42,7 @@ public class YourActivity extends AwesomeSplash {
         configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
         configSplash.setAnimPathStrokeDrawingDuration(3000);
         configSplash.setPathSplashStrokeSize(3); //I advise value be <5
-        configSplash.setPathSplashStrokeColor(getResources().getColor(R.color.accent)); //any color you want form colors.xml
+        configSplash.setPathSplashStrokeColor(getResources().getColor(R.color.primary)); //any color you want form colors.xml
         configSplash.setAnimPathFillingDuration(3000);
         configSplash.setPathSplashFillColor(getResources().getColor(R.color.Wheat)); //path object filling color
 
@@ -54,7 +56,7 @@ public class YourActivity extends AwesomeSplash {
         configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
         Log.d("vince","Your activity initsplash end");
 
-        //animationsFinished();
+        animationsFinished();
 
     }
 
@@ -63,19 +65,21 @@ public class YourActivity extends AwesomeSplash {
 
         //transit to another activity here
         //or do whatever you want
-        /**
+        Log.d("vince","Your activity animationFinished entered");
         final Activity a = this;
-        WeakHandler handler = new WeakHandler();
+        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                ChangeActivityHelper.changeActivity(a, MainActivity.class, true);
-            }
-        }, Constants.SPLASH_DELAY);
-        */
+                Log.d("vince","Your activity animationFinished inside the run after 5000");
+                Intent intent = new Intent(a,test_sample.class);
+                startActivity(intent);
 
-        Log.d("vince","Your activity animationFinished");
-        Intent intent = new Intent(this,test_sample.class);
-        startActivity(intent);
+            }
+        },7000);
+
+        Log.d("vince","Your activity animationFinished finished");
+
+
     }
 }
